11a6ac661bae2767b32402113e6f913363629125
aulos <aulospl@gmail.com>
2016-10-05 15:39:40 -0300

    texture manager added to game.hpp

--- game.hpp
+++ game.hpp
@@ -3,14 +3,21 @@
 
 #include <stack>
 #include <SFML/Graphics.hpp>
-
+#include "texture_manager.hpp"
 //Fixes cyclic dependecy of GameState class
 class GameState;
 
 class Game{
+private:
+    void loadTextures();
+
 public:
     std::stack<GameState*> states;
+
     sf::RenderWindow window;
+    TextureManager texmgr;
+    sf::Sprite background;
+
 
     void pushState(GameState* state);
     void popState();
